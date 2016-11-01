3a8f5584a613855068db83141b1566dc1a5604c9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-11-01 15:56:04 +0900

    use `no-useless-return` introduced in ESLint 3.9.0
    http://eslint.org/blog/2016/10/eslint-v3.9.0-released
    http://eslint.org/docs/rules/no-useless-return
    https://github.com/eslint/eslint/commit/d9335166e0d417ae74bf380e821bf16a79eb90aa

--- index.js
+++ index.js
@@ -276,6 +276,7 @@ module.exports = {
     'no-useless-call': 'error',
     'no-useless-concat': 'error',
     'no-useless-escape': 'error',
+    'no-useless-return': 'error',
     'no-void': 'error',
     'no-warning-comments': 'error',
     'no-with': 'error',
